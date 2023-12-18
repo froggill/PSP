e937c94158e5c922100d0e951303b96e455b9704
Ekaterina Zubar <144928898+froggill@users.noreply.github.com>
2023-12-18 15:17:26 +0300

    8 лабораторная

---
+++ src/main/com.makkras/java/servlet/AddMenuItemServlet.java
@@ -0,0 +1,36 @@
+package java.servlet;
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.dbHandler.MenuDbService;
+import java.exception.CustomException;
+import java.io.IOException;
+
+@WebServlet("/addMenuItem")
+public class AddMenuItemServlet extends HttpServlet {
+    private static Logger logger = LogManager.getLogger();
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        HttpSession session = req.getSession();
+        logger.info(session.getAttribute("name"));
+        logger.info(session.getAttribute("password"));
+
+        try {
+            if(!MenuDbService.checkIfMenuItemAlreadyExists(req.getParameter("nameMenuItemToAdd"))){
+                MenuDbService.addMenuItemToDb(req.getParameter("nameMenuItemToAdd"), Integer.parseInt(req.getParameter("priceMenuItemToAdd")));
+            }
+            else {
+                session.setAttribute("error","Такое блюдо уже добавлено!");
+            }
+            resp.sendRedirect("/jsp/admMainMenuForm.jsp");
+        } catch (CustomException e) {
+            logger.error(e.getMessage());
+        }
+    }
+}
---
+++ src/main/com.makkras/java/servlet/ShowAllMenuServlet.java
@@ -0,0 +1,48 @@
+package java.servlet;
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.dbHandler.MenuDbService;
+import java.entity.MenuItem;
+import java.exception.CustomException;
+import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebServlet("/showMenu")
+public class ShowAllMenuServlet extends HttpServlet {
+    private static Logger logger = LogManager.getLogger();
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        HttpSession session = req.getSession();
+        logger.info(session.getAttribute("name"));
+        logger.info(session.getAttribute("password"));
+        try {
+            ResultSet resultSet = MenuDbService.showAllMenuForAdmin();
+            List<MenuItem> menuItemList = new ArrayList<>();
+            while (resultSet.next()){
+                logger.info(resultSet.getInt(1));
+                logger.info(resultSet.getString(2));
+                logger.info(resultSet.getInt(3));
+
+                MenuItem menuItem = new MenuItem(resultSet.getInt(1), resultSet.getString(2), resultSet.getInt(3));
+                menuItemList.add(menuItem);
+            }
+            session.setAttribute("ResultList", menuItemList);
+            resp.sendRedirect("/jsp/menuList.jsp");
+        } catch (CustomException e) {
+            logger.error(e.getMessage());
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+}
